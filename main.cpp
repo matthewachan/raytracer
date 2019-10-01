361d64ec0b0d47cfcda172654f7fdeb7647663d4
Matthew Chan <matthew.a.chan@gmail.com>
2019-09-30 20:53:47 -0400

    Implement part 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <Eigen/Dense>
+#include "ray.hpp"
+using namespace Eigen;
+
+Vector3f color(const ray& r)
+{
+	Vector3f unit_direction = r.direction().normalized();
+	float t = 0.5f * (unit_direction.y() + 1.0f);
+	return (1.0f - t) * Vector3f(1.0f, 1.0f, 1.0f) + t * Vector3f(0.5f, 0.7f, 1.0f);
+}
 
 int main()
 {
@@ -8,14 +18,19 @@ int main()
 	int nx = 200;
 	int ny = 100;
 	output << "P3\n" << nx << " " << ny << "\n255\n";
+	Vector3f lower_left_corner(-2.0f, -1.0f, -1.0f);
+	Vector3f horizontal(4.0f, 0.0f, 0.0f);
+	Vector3f vertical(0.0f, 2.0f, 0.0f);
+	Vector3f origin(0.0f, 0.0f, 0.0f);
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; ++i) {
-			float r = float(i) / nx;
-			float g = float(j) / ny;
-			float b = 0.2f;
-			int ir = int (255.99 * r);
-			int ig = int (255.99 * g);
-			int ib = int (255.99 * b);
+			float u = float(i) / nx;
+			float v = float(j) / ny;
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			Vector3f col = color(r);
+			int ir = int (255.99 * col[0]);
+			int ig = int (255.99 * col[1]);
+			int ib = int (255.99 * col[2]);
 			output << ir << " " << ig << " " << ib << "\n";
 		}
 	}
