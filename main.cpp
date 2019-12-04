eb9a8362b520fb0fef404e630e57957166d398ab
Matthew Chan <matthew.a.chan@gmail.com>
2019-12-04 18:51:25 -0500

    Working Cornell box with triangle

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <Eigen/Dense>
 #include <limits>
 #include <random>
+#include <thread>
 
 #include "camera.hpp"
 #include "hittable_list.hpp"
@@ -40,19 +41,18 @@ Vector3f color(const ray& r, hittable *world, int depth)
 }
 
 hittable *cornell_box() {
-    hittable **list = new hittable*[6];
+    hittable **list = new hittable*[7];
     int i = 0;
     material *red = new lambertian(Vector3f(0.65, 0.05, 0.05));
     material *white = new lambertian(Vector3f(0.73, 0.73, 0.73));
-    material *green = new lambertian(Vector3f(0.12, 0.45, 0.15));
-    material *light = new diffuse_light(Vector3f(15, 15, 15));
-
-    list[i++] = new flip_normals(new yz_plane(0, 555, 0, 555, 555, green));
+    material *green = new lambertian(Vector3f(0.12, 0.45, 0.15)); material *light = new diffuse_light(Vector3f(15, 15, 15)); list[i++] = new flip_normals(new yz_plane(0, 555, 0, 555, 555, green));
     list[i++] = new yz_plane(0, 555, 0, 555, 0, red);
     list[i++] = new xz_plane(213, 343, 227, 332, 554, light);
     list[i++] = new flip_normals(new xz_plane(0, 555, 0, 555, 555, white));
     list[i++] = new xz_plane(0, 555, 0, 555, 0, white);
     list[i++] = new flip_normals(new xy_plane(0, 555, 0, 555, 555, white));
+    /* list[i++] = new sphere(Vector3f(278, 278, 278), 50, red); */
+    list[i++] = new flip_normals(new triangle(Vector3f(150, 150, 150), Vector3f(500,500,150), Vector3f(500,150,150), red));
 
     return new hittable_list(list,i);
 }
@@ -61,44 +61,48 @@ int main()
 {
 	std::ofstream output;
 	output.open("output.ppm");
-	int nx = 200;
-	int ny = 200;
-	int ns = 100;
+	int nx = 100;
+	int ny = 100;
+	int ns = 50;
 	output << "P3\n" << nx << " " << ny << "\n255\n";
 
-	Vector3f lookfrom(0, 0, 1);
-	Vector3f lookat(0, 0, -1);
 
 	/* Cornell box camera settings */
-	/* Vector3f lookfrom(278, 278, -800); */
-	/* Vector3f lookat(278,278,0); */
-	/* float dist_to_focus = 10.0; */
-	/* float aperture = 0.0; */
-	/* float vfov = 40.0; */
+	Vector3f lookfrom(278, 278, -800);
+	Vector3f lookat(278,278,0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.0;
+	float vfov = 40.0;
 
-	/* camera cam(lookfrom, lookat, Vector3f(0,1,0), vfov, float(nx)/float(ny)); */
-	camera cam(Eigen::Vector3f(0,0,1), Eigen::Vector3f(0,0,-1), Eigen::Vector3f(0,1,0), 90, float(nx)/ny);
+	camera cam(lookfrom, lookat, Vector3f(0,1,0), vfov, float(nx)/float(ny));
+	hittable *world = cornell_box();
 
-	hittable ** list = new hittable*[4];
+	/* Vector3f lookfrom(0, 0, 1); */
+	/* Vector3f lookat(0, 0, -1); */
+	/* camera cam(Eigen::Vector3f(0,0,1), Eigen::Vector3f(0,0,-1), Eigen::Vector3f(0,1,0), 90, float(nx)/ny); */
+	/* int n = 4; */
+	/* hittable ** list = new hittable*[n]; */
 	/* list[0] = new sphere(Vector3f(0,-1000, 0), 1000, new lambertian(Vector3f(0.1, 0.2, 0.5))); */
 	/* list[1] = new sphere(Vector3f(0, 2, 0), 2, new lambertian(Vector3f(0.8, 0.8, 0))); */
 	/* list[2] = new sphere(Vector3f(0, 7, 0), 2, */
 	/* 		new diffuse_light(Vector3f(4,4,4))); */
 	/* list[3] = new xyrect(3, 5, 1, 3, -2, */
 	/* 		new diffuse_light(Vector3f(3, 3, 3))); */
-	list[0] = new sphere(Vector3f(0, 0, -1), 0.5, new lambertian(Vector3f(0.1, 0.2, 0.5)));
-	list[1] = new sphere(Vector3f(0, -100.5, -1), 100, new lambertian(Vector3f(0.8, 0.8, 0.0)));
-	list[2] = new triangle(Vector3f(-1, -1, -2), Vector3f(-1, 1, -2), Vector3f(1, 1, -2), new lambertian(Vector3f(1, 0, 0)));
+	/* list[0] = new sphere(Vector3f(0, 0, -1), 0.5, new lambertian(Vector3f(0.1, 0.2, 0.5))); */
+	/* list[1] = new sphere(Vector3f(0, -100.5, -1), 100, new lambertian(Vector3f(0.8, 0.8, 0.0))); */
+	/* list[0] = new triangle(Vector3f(-100, -100, -2), Vector3f(-100, 100, -2), Vector3f(100, 100, -2), new lambertian(Vector3f(1, 0, 0))); */
 	/* list[2] = new sphere(Vector3f(1, 0, -1), 0.5, new metal(Vector3f(0.8, 0.6, 0.2))); */
 	/* list[3] = new sphere(Vector3f(-1, 0, -1), 0.5, new dielectric(1.5)); */
-	list[3] = new sphere(Vector3f(-1, 2, -1), 0.5, new diffuse_light(Vector3f(24,24,24)));
-	/* hittable **list = new hittable*[2]; */
-	/* list[0] = new triangle(Vector3f(0, 2, 0), Vector3f(1, 2, 0), Vector3f(4, 4, 0), new lambertian(Vector3f(0.1, 0.2, 0.5))); */
-	/* list[1] = new sphere(Vector3f(0, 2, 0), 2, new lambertian(Vector3f(0.8, 0.8, 0))); */
-	hittable *world = new hittable_list(list, 4);
 
 
-	/* hittable *world = cornell_box(); */
+
+	/* list[0] = new sphere(Vector3f(-1, 1, -1), 1, new diffuse_light(Vector3f(24,24,24))); */
+	/* list[1] = new triangle(Vector3f(-10, -10, -2), Vector3f(-10, 10, -2), Vector3f(10, 10, -2), new lambertian(Vector3f(1, 0, 0))); */
+	/* list[2] = new xy_plane(-10, 10, -10, 10, -3, new lambertian(Vector3f(0, 1, 0))); */
+	/* list[3] = new sphere(Vector3f(1, -1, -1), 0.5, new dielectric(1.5)); */
+	/* hittable *world = new hittable_list(list, n); */
+
+
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; ++i) {
 			Vector3f col(0, 0, 0);
@@ -106,7 +110,6 @@ int main()
 				float u = float(i + drand48()) / nx;
 				float v = float(j + drand48()) / ny;
 				ray r = cam.get_ray(u, v);
-				Vector3f p = r.point_at_parameter(2.0);
 				col += color(r, world, 0);
 			}
 			col /= float(ns);
--- triangle.hpp
+++ triangle.hpp
@@ -10,6 +10,40 @@ public:
 	triangle(Eigen::Vector3f v0, Eigen::Vector3f v1, Eigen::Vector3f v2, material* m) : v0(v0), v1(v1), v2(v2), mat(m) {};
 	virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const
 	{
+		/* const float EPSILON = 0.0000001; */
+		/* Eigen::Vector3f edge1, edge2, h, s, q; */
+		/* float a,f,u,v; */
+		/* edge1 = v1 - v0; */
+		/* edge2 = v2 - v0; */
+		/* h = r.direction().cross(edge2); */
+		/* a = edge1.dot(h); */
+		/* if (a > -EPSILON && a < EPSILON) */
+		/* 	return false;    // This ray is parallel to this triangle. */
+		/* f = 1.0/a; */
+		/* s = r.origin() - v0; */
+		/* u = f * s.dot(h); */
+		/* if (u < 0.0 || u > 1.0) */
+		/* 	return false; */
+		/* q = s.cross(edge1); */
+		/* v = f * r.direction().dot(q); */
+		/* if (v < 0.0 || u + v > 1.0) */
+		/* 	return false; */
+		/* // At this stage we can compute t to find out where the intersection point is on the line. */
+		/* float t = f * edge2.dot(q); */
+		/* if (t > EPSILON && t < 1/EPSILON) // ray intersection */
+		/* { */
+		/* 	Eigen::Vector3f p = r.point_at_parameter(t); */
+		/* 	rec.t = t; */
+		/* 	rec.mat = mat; */
+		/* 	rec.normal = (edge1.cross(edge2)).normalized(); */
+		/* 	rec.p = p; */
+		/* 	return true; */
+		/* } */
+		/* else // This means that there is a line intersection but not a ray intersection. */
+		/* 	return false; */
+
+
+
 		Eigen::Vector3f v0v1 = v1 - v0; 
 		Eigen::Vector3f v0v2 = v2 - v0; 
 		Eigen::Vector3f pvec = r.direction().cross(v0v2); 
@@ -25,17 +59,14 @@ public:
 		if (v < 0 || u + v > 1) return false;
 
 		float t = v0v2.dot(qvec) * invDet;
+		if (t > tmax || t < tmin) return false;
 		Eigen::Vector3f p = r.point_at_parameter(t);
 		rec.t = t;
 		rec.mat = mat;
-		rec.normal = v0v1.cross(v0v2);
+		rec.normal = (v0v1.cross(v0v2)).normalized();
 		rec.p = p;
 
 		return true;
-
-
-
-
 		/* // Plane defined by vector in the plane and normal */
 		/* Eigen::Vector3f vec1 = v1-v2; */
 		/* Eigen::Vector3f vec2 = v3-v2; */
@@ -63,19 +94,20 @@ public:
 		/* return true; */
 	}
 
-	
+
 
 	virtual bool bounding_box(float t0, float t1, aabb& box) const
 	{
 		// Construct AABB from front, left, bot corner and back, right top corner
 		float xmin, xmax, ymin, ymax, zmin, zmax;
+		float offset = 0.00001;
 		xmin = std::min(std::min(v1[0], v2[0]), v0[0]);
 		xmax = std::max(std::max(v1[0], v2[0]), v0[0]);
 		ymin = std::min(std::min(v1[1], v2[1]), v0[1]);
 		ymax = std::max(std::max(v1[1], v2[1]), v0[1]);
 		zmin = std::min(std::min(v1[2], v2[2]), v0[2]);
 		zmax = std::max(std::max(v1[2], v2[2]), v0[2]);
-		box = aabb(Eigen::Vector3f(xmin, ymin, zmin), Eigen::Vector3f(xmax, ymax, zmax));
+		box = aabb(Eigen::Vector3f(xmin, ymin, zmin-offset), Eigen::Vector3f(xmax, ymax, zmax+offset));
 		return true;
 	}
 	Eigen::Vector3f v0;
